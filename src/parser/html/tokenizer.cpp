b45bf9b0feac556b4a7ff9d6c61f2739928963d1
Saman <m28mahmoodi@gmail.com>
2026-06-05 23:59:25 +0200

    add basic skeleton tokenizer according to the standard. needs testing both for compilaiton and working

--- src/parser/html/tokenizer.cpp
+++ src/parser/html/tokenizer.cpp
@@ -1,5 +1,6 @@
 #include "tokenizer.hpp"
-
+#include <cctype>
+#include <iostream>
 
 std::string Tokenizer::consume_while(std::function<bool(char)> const& fun) const{
     std::string result{""};
@@ -21,6 +22,90 @@ std::string Tokenizer::consume_word() {
     });
 }
 
-Tokenizer::Tokenizer(std::string const& txt): _stream{new InputStream(txt)} {
+Tokenizer::Tokenizer(std::string const& txt): _stream{new InputStream{txt}}, _state{StateMachine::data} {
+    this->_main_executer = {
+        {StateMachine::data, this->open_state()},
+        {StateMachine::end_tag_open, this->end_tag_open_state()},
+        {StateMachine::open_tag, this->tag_open_state()},
+        {StateMachine::end_tag_open, this->end_tag_open_state()}
+    };
+}
 
+void Tokenizer::main_loop() {
+    while (!this->_stream->eof())
+        this->_main_executer[this->_state]();
+}
+
+void Tokenizer::open_state() {
+    auto ch{this->_stream->next()};
+
+    switch (ch) {
+        case '&':
+        //TODO: implement according to the standard
+        break;
+        case '<':
+            this->_state = StateMachine::open_tag;
+        break;
+        //TODO: handle the EOF token and null case
+        default:
+            this->_current_token = ch;
+            std::cout << "emitting token character: " << std::get<Tokens::CharacterToken>(this->_current_token) << std::endl;
+            break;
+    }
+}
+
+void Tokenizer::tag_open_state() {
+    auto ch {this->_stream->peek()};
+
+    if (std::isalpha(static_cast<unsigned char>(ch)))
+        {
+            this->_current_token = Tokens::TagToken{};
+            this->_state = StateMachine::tag_name;
+        }
+
+    switch (ch) {
+        case '/':
+            this->_state = StateMachine::end_tag_open;
+            this->_stream->next();
+        break;
+        /*TODO: implemet rest according to the standards*/
+    
+    }
+}
+
+void Tokenizer::tag_name_state() {
+    auto ch{this->_stream->next()};
+    if (std::isalpha(static_cast<unsigned char>(ch)))
+        {
+            if (std::isupper(static_cast<unsigned char>(ch)))
+                ch = std::tolower(static_cast<unsigned char>(ch));
+            
+            std::get<Tokens::TagToken>(this->_current_token).tag_name += ch;
+        }
+    switch (ch) {
+        case '>':
+            this->_state = StateMachine::data;
+            std::cout << "emitting current tag token: " << std::get<Tokens::TagToken>(this->_current_token).tag_name << std::endl;
+        break;
+        /*TODO: implemet rest according to the standards*/
+    }
+}
+
+void Tokenizer::end_tag_open_state() {
+    auto ch {this->_stream->peek()};
+
+    if (std::isalpha(static_cast<unsigned char>(ch)))
+        {
+            this->_current_token = Tokens::TagToken{};
+            this->_state = StateMachine::tag_name;
+        }
+
+    switch (ch) {
+        case '>':
+            this->_state = StateMachine::data;
+            this->_stream->next();
+        break;
+        /*TODO: implemet rest according to the standards*/
+    
+    }
 }
--- src/parser/html/tokenizer.hpp
+++ src/parser/html/tokenizer.hpp
@@ -3,12 +3,45 @@
 #include "InputStream.hpp"
 #include <memory>
 #include <functional>
+#include <unordered_map>
+#include <variant>
 
+enum class StateMachine {
+    data,
+    open_tag,
+    end_tag_open,
+    tag_name
+};
+
+namespace Tokens {
+    using CharacterToken = char;
+    
+    struct TagToken {
+        std::string tag_name{};
+        /* TODO:: add the other attributes */
+    };
+}
 class Tokenizer{
+
     std::unique_ptr<InputStream> const _stream;
     std::string consume_while(std::function<bool(char)>const&) const;
+
+    /* since the state machine is huge!! for maintainablity is better to use this rather than switch.
+    * however, it can also be implemented with arrays. if things are slow alternetive solutions should be explored.
+    */
+    std::unordered_map<StateMachine, std::function<void()>> _main_executer;
+    StateMachine _state;
+
+    void open_state();
+    void tag_open_state();
+    void tag_name_state();
+    void end_tag_open_state();
+
+    std::variant<Tokens::CharacterToken, Tokens::TagToken> _current_token;
+
     public:
-    Tokenizer(std::string const& txt);
+    void main_loop();
     void remove_spaces() const;
+    Tokenizer(std::string const& txt);
     std::string consume_word();
 };
